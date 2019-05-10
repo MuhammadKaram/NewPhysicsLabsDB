f532d79fcee89a5f9b3a5425d7fe8c18706f4991
ALShaimaa_Karam <alshaimaak.cs@gmail.com>
2019-05-10 17:46:42 +0200

    Custody Done

--- PhysicsLabsDB/Custody/frmCustody.Designer.cs
+++ PhysicsLabsDB/Custody/frmCustody.Designer.cs
@@ -28,27 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // frmCustody
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 22F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSize = true;
-            this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1150, 550);
-            this.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "frmCustody";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "frmLabs";
-            this.ResumeLayout(false);
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCustody));
+			this.lbxEmployee = new System.Windows.Forms.ListBox();
+			this.txtNameEmplyee = new System.Windows.Forms.TextBox();
+			this.grbAddEmployee = new System.Windows.Forms.GroupBox();
+			this.btnAddEmploee = new System.Windows.Forms.Button();
+			this.btnDeleteEmployee = new System.Windows.Forms.Button();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.grbAddEmployee.SuspendLayout();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// lbxEmployee
+			// 
+			this.lbxEmployee.FormattingEnabled = true;
+			this.lbxEmployee.ItemHeight = 19;
+			this.lbxEmployee.Location = new System.Drawing.Point(71, 34);
+			this.lbxEmployee.Name = "lbxEmployee";
+			this.lbxEmployee.Size = new System.Drawing.Size(270, 308);
+			this.lbxEmployee.TabIndex = 1;
+			// 
+			// txtNameEmplyee
+			// 
+			this.txtNameEmplyee.Location = new System.Drawing.Point(71, 39);
+			this.txtNameEmplyee.Name = "txtNameEmplyee";
+			this.txtNameEmplyee.Size = new System.Drawing.Size(270, 26);
+			this.txtNameEmplyee.TabIndex = 3;
+			this.txtNameEmplyee.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtNameEmplyee_KeyDown);
+			// 
+			// grbAddEmployee
+			// 
+			this.grbAddEmployee.Controls.Add(this.txtNameEmplyee);
+			this.grbAddEmployee.Controls.Add(this.btnAddEmploee);
+			this.grbAddEmployee.Location = new System.Drawing.Point(21, 29);
+			this.grbAddEmployee.Name = "grbAddEmployee";
+			this.grbAddEmployee.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+			this.grbAddEmployee.Size = new System.Drawing.Size(364, 98);
+			this.grbAddEmployee.TabIndex = 6;
+			this.grbAddEmployee.TabStop = false;
+			this.grbAddEmployee.Text = "إضافة متعهد";
+			// 
+			// btnAddEmploee
+			// 
+			this.btnAddEmploee.BackgroundImage = global::PhysicsLabsDB.Properties.Resources.iconfinder_sign_add_299068;
+			this.btnAddEmploee.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.btnAddEmploee.Location = new System.Drawing.Point(23, 39);
+			this.btnAddEmploee.Name = "btnAddEmploee";
+			this.btnAddEmploee.Size = new System.Drawing.Size(28, 26);
+			this.btnAddEmploee.TabIndex = 4;
+			this.btnAddEmploee.UseVisualStyleBackColor = true;
+			this.btnAddEmploee.Click += new System.EventHandler(this.btnAddEmploee_Click);
+			// 
+			// btnDeleteEmployee
+			// 
+			this.btnDeleteEmployee.BackgroundImage = global::PhysicsLabsDB.Properties.Resources.iconfinder_error_1646012;
+			this.btnDeleteEmployee.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.btnDeleteEmployee.Location = new System.Drawing.Point(23, 34);
+			this.btnDeleteEmployee.Name = "btnDeleteEmployee";
+			this.btnDeleteEmployee.Size = new System.Drawing.Size(28, 30);
+			this.btnDeleteEmployee.TabIndex = 5;
+			this.btnDeleteEmployee.UseVisualStyleBackColor = true;
+			this.btnDeleteEmployee.Click += new System.EventHandler(this.btnDeleteEmployee_Click);
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.btnDeleteEmployee);
+			this.groupBox1.Controls.Add(this.lbxEmployee);
+			this.groupBox1.Font = new System.Drawing.Font("Arial", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.groupBox1.Location = new System.Drawing.Point(21, 170);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+			this.groupBox1.Size = new System.Drawing.Size(364, 361);
+			this.groupBox1.TabIndex = 7;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "أصحاب العهد الحالية";
+			// 
+			// frmCustody
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSize = true;
+			this.BackColor = System.Drawing.Color.White;
+			this.ClientSize = new System.Drawing.Size(417, 561);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.grbAddEmployee);
+			this.Font = new System.Drawing.Font("Arial", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.MaximizeBox = false;
+			this.MaximumSize = new System.Drawing.Size(433, 600);
+			this.MinimizeBox = false;
+			this.MinimumSize = new System.Drawing.Size(433, 600);
+			this.Name = "frmCustody";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "أصحاب العهد";
+			this.Load += new System.EventHandler(this.frmCustody_Load);
+			this.grbAddEmployee.ResumeLayout(false);
+			this.grbAddEmployee.PerformLayout();
+			this.groupBox1.ResumeLayout(false);
+			this.ResumeLayout(false);
 
         }
 
-        #endregion
-    }
+		#endregion
+		private System.Windows.Forms.ListBox lbxEmployee;
+		private System.Windows.Forms.TextBox txtNameEmplyee;
+		private System.Windows.Forms.Button btnAddEmploee;
+		private System.Windows.Forms.Button btnDeleteEmployee;
+		private System.Windows.Forms.GroupBox grbAddEmployee;
+		private System.Windows.Forms.GroupBox groupBox1;
+	}
 }
---
+++ PhysicsLabsDB/Custody/frmTransferCustody.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhysicsLabsDB.Custody
+{
+	public partial class frmTransferCustody : Form
+	{
+		physics_dbEntities db = new physics_dbEntities();
+
+		public frmTransferCustody()
+		{
+			InitializeComponent();
+		}
+
+		private void btnTransfer_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				DialogResult dialogResult = MessageBox.Show("هل تريد نقل المتعهد", "رسالة تأكيد", MessageBoxButtons.YesNo);
+				if (dialogResult == DialogResult.Yes)
+				{
+					List<devices_tb> OldRespon = db.devices_tb.Where(u => u.lab_name == cmbLab.Text && u.respon == cmbCurrentCustody.Text).ToList();
+
+					foreach (var r in OldRespon)
+					{
+						r.respon = cmbNewCustody.Text;
+					}
+					db.SaveChanges();
+
+					//cmbNewCustody.Text = string.Empty;
+
+					//lbxEmployee.Items.Clear();
+					var CurrentCustody = db.devices_tb.Where(u => u.lab_name == cmbLab.Text).Select(u => u.respon).Distinct().ToList();
+					cmbCurrentCustody.DataSource = CurrentCustody;
+					//cmbNewCustody.DataSource = db.respons.Select(u => u.name).ToList();
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.ToString(), "خطأ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+
+		private void frmTransferCustody_Load(object sender, EventArgs e)
+		{
+			cmbLab.DataSource = db.labs.Select(u => u.lab_name).ToList();
+			//cmbCurrentCustody.DataSource = db.devices_tb.Where(u => u.lab_name == cmbLab.Text).Select(u => u.respon).ToList();
+			//cmbCurrentCustody.DataSource = db.respons.Select(u => u.name).ToList();
+			cmbNewCustody.DataSource = db.respons.Select(u => u.name).ToList();
+		}
+
+		private void cmbLab_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (cmbLab.SelectedItem == null)
+			{
+				cmbCurrentCustody.DataSource = null;
+			}
+			else
+			{
+				var CurrentCustody = db.devices_tb.Where(u => u.lab_name == cmbLab.Text).Select(u => u.respon).Distinct().ToList();
+				cmbCurrentCustody.DataSource = CurrentCustody;
+			}
+		}
+	}
+}
