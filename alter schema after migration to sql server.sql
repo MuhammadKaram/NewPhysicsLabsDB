3ebe825602695916867e5fa242e120b8670a7cd8
MuhammadKaram <m.karamayed@gmail.com>
2019-07-23 21:30:09 +0200

    ready

--- alter schema after migration to sql server.sql
+++
@@ -1,7 +0,0 @@
-alter schema dbo transfer physics_db.device_status;
-alter schema dbo transfer physics_db.devices_tb;
-alter schema dbo transfer physics_db.exp;
-alter schema dbo transfer physics_db.labs;
-alter schema dbo transfer physics_db.respon;
-alter schema dbo transfer physics_db.accounts;
-alter schema dbo transfer physics_db.account_types;
